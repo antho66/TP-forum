b44b6731a4e8932387b78eba1329347d6365aaad
Alfonso Fernandez <le_debutant@yahoo.fr>
2017-07-12 10:39:04 +0200

    reajustement css au niveau d'une table. il faut fermer les tags

--- view/listesujets.php
+++ view/listesujets.php
@@ -6,68 +6,99 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="view/style.css">
     <script src="view/jquery-3.2.1.min.js"></script>
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
+          integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css"
+          integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"
+            integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa"
+            crossorigin="anonymous"></script>
     <title>Tp-forum</title>
 </head>
 <body>
-    <div class="header">
-      <div classe"debut">
-        <img src ="cryptomd.png" class="logbit">
- 
-  <div>
-</div>
-<nav> <buttom class"deconecte">se deconnecter</buttom> </nav>
+<div class="header">
+    <div classe
+    "debut">
+    <img src="cryptomd.png" class="logbit">
+
+    <div>
+    </div>
+    <nav>
+        <buttom class
+        "deconecte">se deconnecter</buttom> </nav>
 
 
+    <div class="main">
 
-<div class="main">
 
+        <table class="forum">
+            <tr class="premier">
+                <th class
+                "main">Catégorie
+                </th>
+                <th class
+                "info">topics
+                </th>
+                <th class
+                "info">Message
+                </th>
+                <th class
+                "message">Dernier message
+                </th>
+            </tr>
 
-<table class="forum">
-   <tr class="premier">
-     <th class"main">Catégorie<th>
-       <th class"info">topics<th>
-       <th class"info">Message<th>
-         <th class"message">Dernier message<th>
-    </tr>
+            <tr>
+                <td class "info">crypto monnaie</td>
+                <td class "info">51112 </td>
+                <td class "info">12/07/2017</td>
+            </tr>
 
-       <tr>
-     <td class"main">
-       <h4>crypto monnaie<h4>
-       <th class"info">51112<th>
-         <th class"info">12/07/2017<th>
-    </tr>
+            <tr>
+                <td class
+                "main">
+                <h4>Catégorie
+                    <h4>
+                        <th class
+                        "info">51112
+                        <th>
+                        <th class
+                        "info">12/07/2017
+                        <th>
+            </tr>
 
-    <tr>
-     <td class"main">
-       <h4>Catégorie<h4>
-       <th class"info">51112<th>
-         <th class"info">12/07/2017<th>
-    </tr>
+            <tr>
+                <td class
+                "main">
+                <h4>Catégorie
+                    <h4>
+                        <th class
+                        "info">51112
+                        <th>
+                        <th class
+                        "info">12/07/2017
+                        <th>
+            </tr>
 
-    <tr>
-     <td class"main">
-       <h4>Catégorie<h4>
-       <th class"info">51112<th>
-         <th class"info">12/07/2017<th>
-    </tr>
+            <tr>
+                <td class
+                "main">
+                <h4>Catégorie
+                    <h4>
+                        <th class
+                        "info">51112
+                        <th>
+                        <th class
+                        "info">12/07/2017
+                        <th>
+            </tr>
+        </table>
 
-    <tr>
-     <td class"main">
-       <h4>Catégorie<h4>
-       <th class"info">51112<th>
-         <th class"info">12/07/2017<th>
-    </tr>
-  </table> 
-
-  </div> 
+    </div>
 
 </div><!--fin de la main-->
 <div class="footer">
-  
+
 </div>
-    
+
 </body>
 </html>
